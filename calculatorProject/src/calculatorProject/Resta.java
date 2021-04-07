acd92bf4eae9ee110d186e5fd06e84169bcff6d7
jc-herrera <jc.herrera@globant.com>
2021-04-06 21:00:10 -0500

    Se crean las clases de mi calculadora básica: suma y resta

---
+++ calculatorProject/src/calculatorProject/Resta.java
@@ -0,0 +1,16 @@
+package calculatorProject;
+
+public class Resta {
+	private int num1;
+	private int num2;
+	
+	public Resta(int num1, int num2) {
+		this.num1=num1;
+		this.num2=num2;
+	}
+	
+	public int restarNumeros() {
+		return this.num1-this.num2;
+	}
+
+}
