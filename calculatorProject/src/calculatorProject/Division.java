d36845cf884e0ee76fa9bf3e804c2e91271740de
jc-herrera <jc.herrera@globant.com>
2021-04-06 21:11:29 -0500

    Se agregan nuevas operaciones: multiplicación y división

---
+++ calculatorProject/src/calculatorProject/Division.java
@@ -0,0 +1,16 @@
+package calculatorProject;
+
+public class Division {
+	private int num1;
+	private int num2;
+	
+	public Division (int num1, int num2) {
+		this.num1=num1;
+		this.num2=num2;
+	}
+	
+	public int dividirNumeros() {
+		return num1/num2;
+	}
+
+}
